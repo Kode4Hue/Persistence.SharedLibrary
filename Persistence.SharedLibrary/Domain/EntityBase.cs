db5e5f2c0f9bdfbdd507658c102896e59d7a6f91
kodehue <hubertgraham@outlook.com>
2022-04-03 13:32:55 -0500

    added initial persistence library

---
+++ Persistence.SharedLibrary/Domain/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using NodaTime;
+using System;
+
+namespace Persistence.SharedLibrary.Domain
+{
+    public class EntityBase
+    {
+        public object Id { get; set; }
+
+        public LocalDateTime CreatedAtUTC { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public LocalDateTime LastModifiedUTC { get; set; }
+
+        public LocalDateTime LastModifiedBy { get; set; }
+    }
+}
